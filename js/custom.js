e37b8301b30dacc93be72470b29b6cfc1cdb32b2
Claire Villard <cvillard@v3d.fr>
2019-11-27 11:15:54 +0100

    Add Events page, Event template and a first blog post for november meetup

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 WebFont.load({
     custom: {
         families: ["Ubuntu:n4", "Font Awesome 5 Brands:n4", "Font Awesome 5 Free:n4,n9"],
-        urls: ["css/custom.css", "css/fontawesome-all.css"]
+        urls: ["/css/custom.css", "/css/fontawesome-all.css"]
     }
 });
