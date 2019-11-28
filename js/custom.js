7911a9ac183d26bb5895b5ced5a2eb2a6c6c0698
Claire Villard <cvillard@v3d.fr>
2019-11-28 11:39:18 +0100

    Add Javascript to hide posts on home page that were in the future at page generation but in the past at page display

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,13 @@ WebFont.load({
         urls: ["/css/custom.css", "/css/fontawesome-all.css"]
     }
 });
+
+function getCompareDate() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    return [year, month, day].join('');
+}
